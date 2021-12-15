77df0afcb7ea727c29f4dc1d15a0ed762fd6d204
cjhmoves33 <cjhmoves33@gmail.com>
2021-12-15 18:10:54 +0900

    Init lint&prettier ESLint, Prettier 설정 했습니다.

--- server/index.js
+++ server/index.js
@@ -1,23 +1,23 @@
-const express = require('express');
-const app = express();
-const cors = require('cors');
-const PORT = process.env.PORT || 3000;
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const PORT = process.env.PORT || 4000
+const morgan = require('morgan')
 
-app.use(express.json());
+app.use(express.json())
 
-app.use(cors({ origin: true }));
+app.use(morgan('dev'))
+app.use(cors({ origin: true }))
 
-app.get('/',(req,res)=>{
+app.get('/', (req, res) => {
   res.send('성공시 체크하기')
 })
 
 app.listen(PORT, () => {
-  console.log(`listening on port ${PORT}`);
-});
+  console.log(`listening on port ${PORT}`)
+})
 
-module.exports = app;
+module.exports = app
 
-
-//squelize 환경구축하고 
+//squelize 환경구축하고
 //연결할떄 DB
-
