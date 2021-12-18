53624d9487376c52d8991a03e4db66b6c463b414
Jun0922 <shaq94@khu.ac.kr>
2021-12-18 19:11:02 +0900

    Add membersAPIs

---
+++ server/controllers/function.js
@@ -0,0 +1,21 @@
+//jwt토큰 만들고 해독하는 함수 작성 예정
+// HA3 참고
+
+const { sign, verify } = require('jsonwebtoken')
+
+module.exports = {
+  makejwt: (data) => {
+    const key = 'chlwjdehqor'
+    const option = { expiresIn: '10d', issuer: 'codecube', subject: 'data' }
+    const token = sign(data, key, option)
+
+    return token
+  },
+  solveToken: (token) => {
+    const key = 'chlwjdehqor'
+
+    const result = verify(token, key)
+
+    return result
+  },
+}
