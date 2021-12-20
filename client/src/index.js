e3183b8517c6865be2bd287b7f320712c56963ac
dh <jres1007@gmail.com>
2021-12-20 18:13:54 +0900

    developloginsingup 회원가입로그인회원정보수정회원탈퇴

--- client/src/index.js
+++ client/src/index.js
@@ -2,12 +2,20 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import './index.css'
 import App from './App'
+import { BrowserRouter } from 'react-router-dom'
 // import reportWebVitals from './reportWebVitals'
 
+// ReactDOM.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>,
+//   document.getElementById('root')
+// )
+
 ReactDOM.render(
-  <React.StrictMode>
+  <BrowserRouter>
     <App />
-  </React.StrictMode>,
+  </BrowserRouter>,
   document.getElementById('root')
 )
 
