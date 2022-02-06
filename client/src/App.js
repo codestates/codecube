62d006ed5fa6e054a0a4ad2beda737cf7580daab
Jun0922 <shaq94@khu.ac.kr>
2022-02-06 20:18:16 +0900

    http/cookie test

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,11 @@ import logo from "./logo.svg";
 import "./App.css";
 import axios from "axios";
 const url = process.env.REACT_APP_SERVER;
+// axios.defaults.withCredentials = true;
 
 function App() {
   const handleClick = () => {
+    console.log("hello");
     axios.get(`${url}/hello`).then((res) => console.log(res.data));
   };
 
--- server/app.js
+++ server/app.js
@@ -2,13 +2,17 @@ const express = require("express");
 const app = express();
 const port = 80;
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+
+app.use(cors({ origin: "http://localhost:3000" }));
+app.use(cookieParser());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
 app.get("/hello", (req, res) => {
-  res.send("world");
+  res.cookie("hello", "world").send("world");
 });
 
 app.listen(port, () => {
